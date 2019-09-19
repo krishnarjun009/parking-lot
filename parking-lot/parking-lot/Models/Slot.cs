938f9411001d61ddbd817e467b43692617e39a40
Krishna009 <krish.arjun009@gmail.com>
2019-09-19 15:03:53 +0530

    added isPremium tag in slot model

--- parking-lot/parking-lot/Models/Slot.cs
+++ parking-lot/parking-lot/Models/Slot.cs
@@ -1,18 +1,20 @@
 ﻿
 namespace parking_lot.Models
 {
-    public class Slot
+    public partial class Slot
     {
-        public int slotId;
-        public int parkingSpaceId;
-        public SlotSize size;
+        public int slotId { get; }
+        public int parkingSpaceId { get; }
+        public SlotSize size { get; }
         public Vehical vehical { get; private set; }
+        public bool isPremium { get; private set; }
 
-        public Slot(int id, int spaceId, SlotSize size)
+        public Slot(int id, int spaceId, SlotSize size, bool isPremium = false)
         {
             this.parkingSpaceId = spaceId;
             this.slotId = id;
             this.size = size;
+            this.isPremium = isPremium;
         }
 
         public void Park(Vehical vehical)
@@ -23,6 +25,11 @@ namespace parking_lot.Models
         public void UnPark()
         {
             this.vehical = null;
+        }
+
+        public void SetPremium(bool premium)
+        {
+            this.isPremium = premium;
         }
     }
 }
