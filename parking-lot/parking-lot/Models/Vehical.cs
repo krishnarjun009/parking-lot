702573e32999f28f334d4316865369429ad92e2d
Krishna009 <krish.arjun009@gmail.com>
2019-09-19 12:44:56 +0530

    abstract vehical model

---
+++ parking-lot/parking-lot/Models/Vehical.cs
@@ -0,0 +1,33 @@
+﻿
+namespace parking_lot.Models
+{
+    public interface IVehical
+    {
+        float GetParkingCost();
+    }
+
+    public abstract class Vehical
+    {
+        public string licenseNumber { get; }
+        public SlotSize size { get; }
+        public Color color { get; }
+        public Slot slot { get; private set; }
+
+        public Vehical(string license, SlotSize size, Color color)
+        {
+            this.licenseNumber = license;
+            this.size = size;
+            this.color = color;
+        }
+
+        public void Park(Slot slot)
+        {
+            this.slot = slot;
+        }
+
+        public void UnPark()
+        {
+            this.slot = null;
+        }
+    }
+}
