98b2680f2261e531bc904abf679a20425a18527b
Krishna009 <krish.arjun009@gmail.com>
2019-09-19 12:45:17 +0530

    parking slot model with base implementation

---
+++ parking-lot/parking-lot/Models/Slot.cs
@@ -0,0 +1,28 @@
+﻿
+namespace parking_lot.Models
+{
+    public class Slot
+    {
+        public int slotId;
+        public int parkingSpaceId;
+        public SlotSize size;
+        private Vehical vehical;
+
+        public Slot(int id, int spaceId, SlotSize size)
+        {
+            this.parkingSpaceId = spaceId;
+            this.slotId = id;
+            this.size = size;
+        }
+
+        public void Park(Vehical vehical)
+        {
+            this.vehical = vehical;
+        }
+
+        public void UnPark()
+        {
+            this.vehical = null;
+        }
+    }
+}
